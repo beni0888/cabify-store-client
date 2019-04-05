5a69cca6eac8f7565d67cd4726d2ed163e4f51cf
Jesús Miguel Benito Calzada <beni0888@hotmail.com>
2019-04-05 14:39:11 +0200

    Add add-bulk command

--- src/main/java/com/jbenitoc/cabifystoreclient/infrastructure/CheckoutShell.java
+++ src/main/java/com/jbenitoc/cabifystoreclient/infrastructure/CheckoutShell.java
@@ -14,9 +14,13 @@ import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
+import static java.util.Arrays.stream;
+
 @ShellComponent
 @AllArgsConstructor
 public class CheckoutShell {
+
+    private static final String LINE_BREAK = System.lineSeparator();
 
     private CreateCart createCart;
     private AddItemToCart addItemToCart;
@@ -33,13 +37,36 @@ public class CheckoutShell {
     public String addItemToCart(
             @ShellOption(value = {"-cart", "-cartId"}, help = "Id of the cart to add the item to")
             String cartId,
-            @ShellOption(value = {"-item", "-itemCode", "-code"}, help = "Code of the item to be added to the cart")
+            @ShellOption(value = {"-item", "-itemCode", "-code"}, help = "Code of the item to be added to the cart (VOUCHER|TSHIRT|MUG)")
             String itemCode,
             @ShellOption(value = {"-quantity"}, defaultValue = "1", help = "Id of the cart to add the item to")
             int quantity) {
 
         addItemToCart.execute(new AddItemToCartCommand(cartId, itemCode, quantity));
         return "Item added to cart";
+    }
+
+    @ShellMethod(value = "Add several items to an existent cart (just one unit per item)", key = {"add-several-items", "add-bulk"})
+    public String addSeveralItemsToCart(
+            @ShellOption(value = {"-cart", "-cartId"}, help = "Id of the cart to add the items to")
+                    String cartId,
+            @ShellOption(value = {"-items", "-itemCodes", "-codes"}, help = "Comma-separated list of item-codes to be added to the cart (VOUCHER,TSHIRT,MUG,TSHIRT)")
+                    String itemCodes) {
+
+        int quantityPerItem = 1;
+        StringBuilder outputBuffer = new StringBuilder();
+        final String itemSplitter = ",";
+        stream(itemCodes.split(itemSplitter)).forEach(code -> {
+            try {
+                addItemToCart.execute(new AddItemToCartCommand(cartId, code, quantityPerItem));
+                outputBuffer.append("Item added: ").append(code).append(LINE_BREAK);
+            } catch (Exception e) {
+                outputBuffer.append("Error adding item: ").append(code)
+                        .append(" - Error: ").append(e.getMessage()).append(LINE_BREAK);
+            }
+        });
+
+        return outputBuffer.toString();
     }
 
     @ShellMethod(value = "Return the total amount for the given cart", key = {"get-cart-total", "total"})
@@ -59,6 +86,5 @@ public class CheckoutShell {
         deleteCart.execute(new DeleteCartQuery(cartId));
         return String.format("Cart deleted: { cartId: %s }", cartId);
     }
-
 
 }
